305bf427a03e1400064c146038dff0c1415b7e80
Wayfreem <wayfreem@163.com>
2018-01-23 15:02:13 +0800

    完善定义公司的表单界面

--- src/app/routes/mdm/define-company/define-company.component.ts
+++ src/app/routes/mdm/define-company/define-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-define-company',
@@ -7,11 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefineCompanyComponent implements OnInit {
 
-  pagetitle: string = "定义公司";
+    form: FormGroup;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+      this.form  = this.fb.group({
+          companyId: ['', [Validators.required,
+              Validators.pattern("^[0-9]*$")]
+          ],
+          companyName: ['', [Validators.required]],
+          shortName: ['', [Validators.required]],
+          countryName: ['', [Validators.required]],
+          provinceName: ['', [Validators.required]],
+          cityName: ['', [Validators.required]],
+          registerAddress: ['', [Validators.required]],
+          taxId: ['', [Validators.required]],
+          bankName: ['', [Validators.required]],
+          accountId: ['', [Validators.required]],
+          contactName: ['', [Validators.required]],
+          contactPhone: ['', [Validators.required]],
+          cellPhoneNumber: ['', [Validators.required]],
+          isValid: [''],
+          englishName: [''],
+          postalCode: [''],
+          supplierCode: [''],
+          customerCode: [''],
+          comments: [''],
+
+      });
+
   }
+
+    get companyId() { return this.form.controls.companyId; }
+    get companyName() { return this.form.controls.companyName; }
+    get shortName() { return this.form.controls.shortName; }
+    get countryName() { return this.form.controls.countryName; }
+    get provinceName() { return this.form.controls.provinceName; }
+    get cityName() { return this.form.controls.cityName; }
+    get registerAddress() { return this.form.controls.registerAddress; }
+    get taxId() { return this.form.controls.taxId; }
+    get bankName() { return this.form.controls.bankName; }
+    get accountId() { return this.form.controls.accountId; }
+
+    _submitForm(){
+        console.log(this.form.value);
+        for (const i in this.form.controls) {
+            this.form.controls[i].markAsDirty();
+        }
+        if (this.form.invalid) return ;
+    }
 
 }

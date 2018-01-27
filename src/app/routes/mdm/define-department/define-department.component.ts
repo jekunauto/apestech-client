c08aea18e6e453c8d964f25c3ce92e8abc5c09e0
Wayfreem <wayfreem@163.com>
2018-01-27 14:59:08 +0800

    修改界面的card-tab标签，使用 tabset 标签

--- src/app/routes/mdm/define-department/define-department.component.ts
+++ src/app/routes/mdm/define-department/define-department.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup} from "@angular/forms";
 import { FormlyFieldConfig, FormlyFormOptions} from "@ngx-formly/core";
+import {NzCardComponent} from "ng-zorro-antd-extra";
 
 @Component({
   selector: 'app-define-department',
@@ -21,73 +22,136 @@ export class DefineDepartmentComponent implements OnInit {
 
     advancedOperation3 = [];
 
-    options: FormlyFormOptions = {};
+    selectIndex = 0;
 
-    feidlArray = [{
-        key: 'deptId',
-        type: 'input',
-        defaultValue: "111",
-        templateOptions: {
-            label: '部门编码',
-            placeholder: '请输入部门编码',
-            required: true,
-            divCol: 8,
-            lblCol: 6,
-            inputCol: 18
-        }
-    }, {
-        key: 'deptName',
-        type: 'input',
-        templateOptions: {
-            label: '部门名称',
-            placeholder: '请输入部门名称',
-            required: true,
-            divCol: 8,
-            lblCol: 6,
-            inputCol: 18
-        }
-    }, {
-        key: 'companyId',
-        type: 'input',
-        templateOptions: {
-            label: '公司编码',
-            placeholder: '请输入公司编码',
-            required: true,
-            divCol: 8,
-            lblCol: 6,
-            inputCol: 18
-        }
-    }, {
-        key: 'companyName',
-        type: 'input',
-        templateOptions: {
-            label: '公司名称',
-            placeholder: '请输入公司名称',
-            required: true,
-            divCol: 8,
-            lblCol: 6,
-            inputCol: 18
-        }
-    }, {
-        key: 'isValid',
-        type: 'checkbox',
-        defaultValue: true,
-        templateOptions: {
-            label: '有效标记',
-            divCol: 8,
-            lblCol: 6,
-            inputCol: 18
-        }
-    }];
+    queryOptions: FormlyFormOptions = {};
+    editOptions: FormlyFormOptions = {};
 
-    formFeild0: FormlyFieldConfig[] = this.feidlArray;
-    formFeild1: FormlyFieldConfig[] = this.feidlArray;
+    queryArray = [
+        {
+            key: 'deptId',
+            type: 'input',
+            defaultValue: "111",
+            templateOptions: {
+                label: '部门编码',
+                placeholder: '请输入部门编码',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'deptName',
+            type: 'input',
+            templateOptions: {
+                label: '部门名称',
+                placeholder: '请输入部门名称',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'companyId',
+            type: 'input',
+            templateOptions: {
+                label: '公司编码',
+                placeholder: '请输入公司编码',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'companyName',
+            type: 'input',
+            templateOptions: {
+                label: '公司名称',
+                placeholder: '请输入公司名称',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'isValid',
+            type: 'checkbox',
+            defaultValue: true,
+            templateOptions: {
+                label: '有效标记',
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }
+    ];
 
+    editArray = [
+        {
+            key: 'deptId',
+            type: 'input',
+            defaultValue: "111",
+            templateOptions: {
+                label: '部门编码',
+                placeholder: '请输入部门编码',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'deptName',
+            type: 'input',
+            templateOptions: {
+                label: '部门名称',
+                placeholder: '请输入部门名称',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'companyId',
+            type: 'input',
+            templateOptions: {
+                label: '公司编码',
+                placeholder: '请输入公司编码',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'companyName',
+            type: 'input',
+            templateOptions: {
+                label: '公司名称',
+                placeholder: '请输入公司名称',
+                required: true,
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }, {
+            key: 'isValid',
+            type: 'checkbox',
+            defaultValue: true,
+            templateOptions: {
+                label: '有效标记',
+                divCol: 8,
+                lblCol: 6,
+                inputCol: 18
+            }
+        }
+    ];
+
+    formFeild0: FormlyFieldConfig[] = this.queryArray;
+    formFeild1: FormlyFieldConfig[] = this.editArray;
 
   constructor() { }
 
   ngOnInit() {
-
+      this.getData();
   }
 
     resetForm(){
@@ -97,8 +161,64 @@ export class DefineDepartmentComponent implements OnInit {
     _search(){
       console.log( "_search" );
       console.log(this.queryForm.value);
+    }
 
+    getData(){
+      let data = [{
+          index: 1,
+          deptId: "0001",
+          deptName: "IT部门",
+          companyId: "0001",
+          companyName: "集群车宝",
+          isValid: true,
+          inputPerson: "admin",
+          inputDate: "2018-1-1"
+      },{
+          index: 2,
+          deptId: "0001",
+          deptName: "ERP部门",
+          companyId: "0001",
+          companyName: "集群车宝",
+          isValid: false,
+          inputPerson: "admin",
+          inputDate: "2018-1-1"
+      },{
+          index: 3,
+          deptId: "0001",
+          deptName: "HR部门",
+          companyId: "0001",
+          companyName: "集群车宝",
+          isValid: true,
+          inputPerson: "admin",
+          inputDate: "2018-1-1"
+      },{
+          index: 4,
+          deptId: "0001",
+          deptName: "法务部门",
+          companyId: "0001",
+          companyName: "集群车宝",
+          isValid: true,
+          inputPerson: "admin",
+          inputDate: "2018-1-1"
+      }];
+      this.advancedOperation3 = data;
+    }
 
+    _editRowData(rowData: any){
+      console.log(rowData);
+      console.log(this);
+
+      this.editForm.patchValue(rowData);
+
+      this.selectIndex = 1;
+    }
+
+    _onQueryTabClick(){
+      this.selectIndex = 0;
+    }
+
+    _onEditTabClick(){
+      this.selectIndex = 1;
     }
 
     _submit(){

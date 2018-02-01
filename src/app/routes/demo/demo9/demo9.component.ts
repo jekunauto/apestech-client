2752ce010a142ddf55506c0e3732b6dfc5943667
Wayfreem <wayfreem@163.com>
2018-02-01 14:51:01 +0800

    完善 dialog传入数据生成界面

--- src/app/routes/demo/demo9/demo9.component.ts
+++ src/app/routes/demo/demo9/demo9.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatDialog} from "@angular/material";
 import {DialogOverview} from "../dialog/dialog-overview";
+import ProficiencyFilter from "../gridComponent/filters/proficiencyFilter";
 
 @Component({
   selector: 'app-demo9',
@@ -32,7 +33,6 @@ export class Demo9Component implements OnInit {
             isStore: [false],
             isBase: [false],
         });
-
     }
 
     _submit(){
@@ -40,22 +40,52 @@ export class Demo9Component implements OnInit {
     }
 
     onSearchAddress(){
+        // 检查是否可以打开弹出框
+
+        // 定义查询条件
+        let conditionField = [
+            { key: 'addressId', type: 'input', defaultValue: this.form.value["addressId"],
+                templateOptions: { label: '地点编码', placeholder: '请输入地点编码',}
+            },
+            { key: 'addressName', type: 'input',
+                templateOptions: {label: '地点名称', placeholder: '请输入地点名称',}
+            },
+            { key: 'companyId', type: 'input',
+                templateOptions: {label: '公司编码', placeholder: '请输入公司编码',}
+            },
+            { key: 'companyName', type: 'input',
+                templateOptions: {label: '公司名称', placeholder: '请输入公司名称',}
+            }
+        ];
+
+        // grid展示的列头
+        let columnDefs = [
+            { headerName: '', width: 30, checkboxSelection: true, suppressMenu: true, pinned: true },
+            { headerName: "名称", field: "name", width: 150, pinned: true },
+            { headerName: "城市", field: "country", width: 150 },
+            { headerName: "能力", field: "proficiency", width: 120, filter: ProficiencyFilter },
+            { headerName: "电话", field: "mobile", width: 150, filter: 'text'},
+            { headerName: "座机", field: "landline", width: 150, filter: 'text'},
+            { headerName: "地址", field: "address", width: 500, filter: 'text'}
+        ];
+
         let dialogRef = this.dialog.open(DialogOverview, {
-            width: '900px',
             data: {
                 title: "查询地点",
-                name: this.form.value["addressId"],
-                animal: "1111"
+                conditionField: conditionField,
+                rowSelection: "multiple", // 定义选择模式，单选或者多选
+                columnDefs: columnDefs,
+                url: "queryAddress.action",
+                result: []
             }
         });
 
-        dialogRef.afterOpen().subscribe( () =>{
+        dialogRef.afterOpen().subscribe( () => {
             console.log("afterOPen")
         });
 
         dialogRef.afterClosed().subscribe(result => {
-            console.log('The dialog was closed');
-            console.log(result);
+            this.form.patchValue({addressName: JSON.stringify(result)})
         });
     }
 }

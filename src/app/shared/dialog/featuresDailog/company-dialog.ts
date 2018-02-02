21948714de0dc7408e001b7f8a6d09921494dc95
Wayfreem <wayfreem@163.com>
2018-02-02 14:15:53 +0800

    将翻译的json单独放入config服务中

--- src/app/shared/dialog/featuresDailog/company-dialog.ts
+++ src/app/shared/dialog/featuresDailog/company-dialog.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from "@angular/core";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {GridOptions} from "ag-grid";
 import {FormGroup} from "@angular/forms";
+import {GridConfigService} from "@core/config/grid-config-service.service";
 
 @Component({
     selector: 'company-dialog',
@@ -24,7 +25,8 @@ export class CompanyDialog {
     columnDefs: any[];
 
     constructor(public dialogRef: MatDialogRef<CompanyDialog>,
-                @Inject(MAT_DIALOG_DATA) public data: any) {
+                @Inject(MAT_DIALOG_DATA) public data: any,
+                gridConfigService: GridConfigService) {
 
         this.conditionField = [
             { key: 'companyId', type: 'input',
@@ -46,82 +48,8 @@ export class CompanyDialog {
         // 选择模式默认为 single
         this.rowSelection = data.rowSelection? data.rowSelection :"single" ;
 
-        this.gridOptions.localeText =  {
-            page: '页',
-            more: '更多',
-            to: '到',
-            of: 'Of',
-            next: '下一页',
-            last: '最后一页',
-            first: '第一页',
-            previous: '上一页',
-            loadingOoo: '加载中...',
-            // for set filter
-            selectAll: '选择所有',
-            searchOoo: 'daSearch...',
-            blanks: '空',
-            // for number filter and text filter
-            filterOoo: '过滤...',
-            applyFilter: 'daApplyFilter...',
-            // for number filter
-            equals: '等于',
-            notEqual: '不等于',
-            lessThanOrEqual: '小于或等于',
-            greaterThanOrEqual: '大于或等于',
-            inRange: '区间',
-            lessThan: '小于',
-            greaterThan: '大于',
-            // for text filter
-            contains: '包含',
-            notContains: '不包含',
-            startsWith: '前匹配',
-            endsWith: '后匹配',
-            // the header of the default group column
-            group: 'laGroup',
-            // tool panel
-            columns: '列',
-            rowGroupColumns: 'laPivot Cols',
-            rowGroupColumnsEmptyMessage: 'la please drag cols to group',
-            valueColumns: 'laValue Cols',
-            pivotMode: 'laPivot-Mode',
-            groups: 'laGroups',
-            values: '值',
-            pivots: 'laPivots',
-            valueColumnsEmptyMessage: 'la drag cols to aggregate',
-            pivotColumnsEmptyMessage: 'la drag here to pivot',
-            // other
-            noRowsToShow: '无数据',
-            // enterprise menu
-            pinColumn: '冻结列',
-            valueAggregation: 'laValue Agg',
-            autosizeThiscolumn: '自动调整当前列列宽',
-            autosizeAllColumns: '自动调整所有列列宽',
-            groupBy: '分组',
-            ungroupBy: '取消分组',
-            resetColumns: '取消列设置',
-            expandAll: '全部展开',
-            collapseAll: '全部折叠',
-            toolPanel: '工具栏',
-            export: '导出',
-            csvExport: '导出csv',
-            excelExport: '导出excel',
-            // enterprise menu pinning
-            pinLeft: '冻结该列居左 <<',
-            pinRight: '冻结该列居右 >>',
-            noPin: '取消冻结 <>',
-            // enterprise menu aggregation and status panel
-            sum: '合计',
-            min: '最小值',
-            max: '最大值',
-            none: '空',
-            count: '统计',
-            average: '平均值',
-            // standard menu
-            copy: '复制',
-            ctrlC: 'ctrl + C',
-            paste: '粘贴',
-            ctrlV: 'ctrl + V'
-        };
+        //加载翻译的内容
+        this.gridOptions.localeText = gridConfigService.getLocaleText();
     }
 
     onNoClick(): void {

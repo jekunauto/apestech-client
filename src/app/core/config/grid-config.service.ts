15cb1da817929870c0b95c556aca2b0aa2927784
Wayfreem <wayfreem@163.com>
2018-02-10 15:52:26 +0800

    增加日期组件

--- src/app/core/config/grid-config.service.ts
+++ src/app/core/config/grid-config.service.ts
@@ -116,3 +116,29 @@ export function numberValueParser(params) {
     return value;
 }
 
+export function dateValueParser(params){
+    let value: any;
+    if( params.value ){
+        value = dateFormat(params.value, "yyyy-MM-dd");
+    }
+    return value;
+}
+
+
+//(new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+//(new Date()).Format("yyyy-M-d h:m:s.S")	  ==> 2006-7-2 8:9:4.18
+function dateFormat(date, fmt) {
+    var o = {
+        "M+": date.getMonth() + 1, //月份
+        "d+": date.getDate(), //日
+        "h+": date.getHours(), //小时
+        "m+": date.getMinutes(), //分
+        "s+": date.getSeconds(), //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}

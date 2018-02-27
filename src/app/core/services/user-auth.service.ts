e1c068d35dbb7955c7aa1b97d0a789e3541d166d
Wayfreem <wayfreem@163.com>
2018-02-27 10:33:08 +0800

    格式化代码，去掉多余的console.log

--- src/app/core/services/user-auth.service.ts
+++ src/app/core/services/user-auth.service.ts
@@ -7,7 +7,8 @@ export class UserAuthService {
     public userKey: UserKeyType = 'currentUser';
     public storage: Storage = sessionStorage;
 
-  constructor() { }
+    constructor() {
+    }
 
     /**
      * 存储用户信息
@@ -19,38 +20,35 @@ export class UserAuthService {
     /**
      * 移除用户信息
      */
-  public removeUserInfo() {
+    public removeUserInfo() {
         this.storage.removeItem(this.userKey);
-  }
-
-  /**
-   * @return {(UserInfoModel | null)} userInfoObj||null - 获取存储在storage中的哦用户信息
-   */
-  public getUserInfoObj(): UserInfoModel | null {
-    try {
-        const userInfo = this.storage.getItem(this.userKey);
-        const userInfoObj = JSON.parse(userInfo);
-      return !!userInfoObj ? userInfoObj : null;
-    } catch (e) {
-      return null;
     }
-  }
 
-  /**
-   * 判断是否处于登录状态
-   */
-  public isLogined() {
-      debugger;
-      console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa');
-      console.log(this.storage.getItem(this.userKey));
-      return !!this.storage.getItem(this.userKey);
-  }
+    /**
+     * @return {(UserInfoModel | null)} userInfoObj||null - 获取存储在storage中的哦用户信息
+     */
+    public getUserInfoObj(): UserInfoModel | null {
+        try {
+            const userInfo = this.storage.getItem(this.userKey);
+            const userInfoObj = JSON.parse(userInfo);
+            return !!userInfoObj ? userInfoObj : null;
+        } catch (e) {
+            return null;
+        }
+    }
 
-  /**
-   * 获取当前用户token
-   * @return token - token
-   */
-  public getToken() {
-      return !!this.getUserInfoObj() ? this.getUserInfoObj().token : null;
-  }
+    /**
+     * 判断是否处于登录状态
+     */
+    public isLogined() {
+        return !!this.storage.getItem(this.userKey);
+    }
+
+    /**
+     * 获取当前用户token
+     * @return token - token
+     */
+    public getToken() {
+        return !!this.getUserInfoObj() ? this.getUserInfoObj().token : null;
+    }
 }

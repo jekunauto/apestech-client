44306cec92c51226abd4a4140ae6aa8b6949eb52
Wayfreem <wayfreem@163.com>
2018-02-28 16:17:01 +0800

    增加grid render select

---
+++ src/app/shared/grid/cell-render/render-select.component.ts
@@ -0,0 +1,28 @@
+import {Component} from "@angular/core";
+import {ICellRendererAngularComp} from "ag-grid-angular";
+
+@Component({
+    selector: 'render-select',
+    template: ` 
+       <div>{{value}}</div>
+    `
+})
+export class RenderSelectComponent implements ICellRendererAngularComp {
+
+    private value: any;
+    private params: any;
+
+    constructor(){ }
+
+    agInit(params: any): void {
+
+        this.params = params;
+        if (params.value){
+            this.value = params.colDef.cellEditorParams.value.label;
+        }
+    }
+
+    refresh(params: any): boolean {
+        return false;
+    }
+}

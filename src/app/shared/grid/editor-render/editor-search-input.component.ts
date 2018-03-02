a663be9242ac58d08453e1234ea56f10657d2d34
Wayfreem <wayfreem@163.com>
2018-03-02 17:18:19 +0800

    处理数据回填之后，新增的一行的时候，新的行还保留之前的值

--- src/app/shared/grid/editor-render/editor-search-input.component.ts
+++ src/app/shared/grid/editor-render/editor-search-input.component.ts
@@ -50,23 +50,28 @@ export class EditorSearchInputComponent implements ICellEditorAngularComp {
         });
 
         dialogRef.afterClosed().subscribe(result => {
-
+            debugger;
             if (result) {
                 /** 遇到的问题记录：
                  * 1.需要给 当前的 value 赋值，不然 grid 中的当前的 value 没有值
                  * 2.不能将 result 直接赋值给 RowData，会报 找不到 object 错
                  * 3.循环的赋值的时候，result中没有key的值时，应取data中的值，不然数据会被清理掉
+                 * 4.通过行模型去更新数据，不会对grid的model造成污染
                  */
                 let resultData = result[0];
                 let nodeId = this.params.column.getColId();
                 let data = this.params.node.data;
+                let rowIndex = this.params.rowIndex;
 
                 for(let key in data){
                     data[key] = !!resultData[key] ? resultData[key] : data[key];
                 }
                 this.value = resultData[nodeId];
 
-                this.params.api.updateRowData({update: [data]});
+                let rowNode = this.params.api.getDisplayedRowAtIndex(rowIndex);
+                rowNode.setData(data);
+
+                this.params.api.stopEditing();  // 加上这行代码会自动触发 调用类中的 onCellValueChanged，即数据回填之后立马触发 onCellValueChanged数据检查
             }
         });
     }
